822dd70b94f3f154d7862748ad7fdd347497e532
terocar <teresa.rovira.7e7@itb.cat>
2023-12-02 23:18:56 +0100

    TRC

---
+++ backend/prova.py
@@ -0,0 +1,19 @@
+# Python program to read
+# json file
+
+import json
+
+# Opening JSON file
+f = open('data.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+# Iterating through the json
+# list
+for i in data['emp_details']:
+    print(i)
+
+# Closing file
+f.close()
